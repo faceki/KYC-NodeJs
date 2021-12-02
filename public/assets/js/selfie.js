7f7ab417ff60f40261c8a5fba276e1c36841d585
gitlab-nabin <nabin@digitalaptech.com>
2021-12-02 16:50:04 +0530

    selfie issue 3

--- public/assets/js/selfie.js
+++ public/assets/js/selfie.js
@@ -2,57 +2,55 @@ const pathUrl = window.location.pathname;
 const baseUrl = window.location.origin+"/";
 var video = document.getElementById("video");
 const messageOne = document.querySelector("#message-1");
-// const messageTwo = document.querySelector('#message-2');
+
 button_callback();
 
-function button_callback() {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        video: {
-          facingMode: "user",
-        },
-      })
-      .then(function (stream) {
-        video.srcObject = stream;
-        video.setAttribute("autoplay", "");
-        video.setAttribute("muted", "");
-        video.setAttribute("playsinline", "");
-        video.play();
-      })
-      .catch(function (err0r) {
-        console.log("Something went wrong!");
-      });
-  }
+function button_callback(camera_type) {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        var options = {
+                audio: false,
+                video: {
+                    facingMode: 'user',
+                },
+        };
+        navigator.mediaDevices.getUserMedia(options).then(function(stream) {
+            video.srcObject = stream;
+            video.setAttribute('autoplay', '');
+            video.setAttribute('muted', '');
+            video.setAttribute('playsinline', '');
+            video.play();
+        }).catch(function (err0r) {
+            console.log("Something went wrong!");
+        });
+    }
 }
 
 function stop(e) {
-  var stream = video.srcObject;
-  var tracks = stream.getTracks();
+    var stream = video.srcObject;
+    var tracks = stream.getTracks();
 
-  for (var i = 0; i < tracks.length; i++) {
-    var track = tracks[i];
-    track.stop();
-  }
+    for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+        track.stop();
+    }
 
-  video.srcObject = null;
+    video.srcObject = null;
 }
 
-let imageData = "";
+let imageData = ''
 
-function takeASnap(vid) {
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
-  canvas.width = vid.videoWidth;
-  canvas.height = vid.videoHeight;
-  ctx.drawImage(vid, 0,0); 
-  imageData = canvas.toDataURL('image/jpeg', 1.0)
-  sessionStorage.setItem("selfie_image", imageData);
-  console.log('data333',sessionStorage.getItem("selfie_image"));
-  return new Promise((res, rej) => {
-    canvas.toBlob(res, "image/jpeg");
-  });
+function takeASnap(vid){
+    let canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext('2d'); 
+    canvas.width = vid.videoWidth;
+    canvas.height = vid.videoHeight;
+    ctx.drawImage(vid, 0,0); 
+    imageData = canvas.toDataURL('image/jpeg', 1.0)
+    sessionStorage.setItem("selfie_image",imageData);
+    console.log('selfie_image',sessionStorage.getItem('selfie_image'));
+    return new Promise((res, rej)=>{
+        canvas.toBlob(res, 'image/jpeg'); 
+    });
 }
 
 function callApi(img_type){
@@ -60,8 +58,6 @@ function callApi(img_type){
     document.getElementById('capture').style.display = "none";
     video.pause(); 
     takeASnap(video).then(blob =>{
-        let front_img = sessionStorage.getItem('doc_front_image');
-        let back_img = sessionStorage.getItem('doc_back_image');
         let selfie_image = sessionStorage.getItem('selfie_image');
         const pathUrlArray1 = pathUrl.split("/");
         var pathUrlArray = pathUrlArray1.filter(function (el) {
@@ -69,6 +65,6 @@ function callApi(img_type){
         });
         pathUrlArray.pop();
         const newPath=pathUrlArray.join("/");
-        window.location.href=baseUrl+"verifying";
+        //window.location.href=baseUrl+"verifying";
     });
 }
