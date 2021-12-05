c032cea1aa7ccaacefa12b55f859513da8c6c3b4
gitlab-nabin <nabin@digitalaptech.com>
2021-12-05 14:51:43 +0530

    image issue solve

--- public/assets/js/kyc.js
+++ public/assets/js/kyc.js
@@ -2,49 +2,46 @@ sessionStorage.removeItem("front_img");
 sessionStorage.removeItem("back_img");
 const pathUrl = window.location.pathname;
 const baseUrl = window.location.origin+"/";
-let is_mobile_device = false;
 
+var idscannerpage = document.getElementById("idscannerpage");
+var selfiepage = document.getElementById("selfiepage");
+var verifypage = document.getElementById("verifypage");
+var successfullpage = document.getElementById("successfullpage");
+var extrapage = document.getElementById("extrapage");
+
+var front_image='';
+var back_image='';
+var selfie_image='';
+let is_mobile_device = false;
 (function(a) {
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
-    is_mobile_device = true;
+        is_mobile_device = true;
 })(navigator.userAgent || navigator.vendor || window.opera);
 
-var widthOfCrop = 555; // width of frame where we have to fit ID for desktop
-var heightOfCrop = 450; // height of frame where we have to fit ID for desktop
-var xCrop = 355; // x axis distance of where we start cropping
-var yCrop = 250; // y axis distance of where we start cropping
-
-if(is_mobile_device==true){
-  widthOfCrop = 650; // width of frame where we have to fit ID for mobile
-  heightOfCrop = 500; // height of frame where we have to fit ID for mobile
-  xCrop = 40; // x axis distance of where we start cropping
-  yCrop = 200; 
-}
-
-(function() {
-    var camera = document.getElementById("camera");
-    var result = document.getElementById("result");
-    var video = document.getElementById("video");
-    var canvas = document.getElementById("canvas");
-    var photo = document.getElementById("photo");
-    var startbutton = document.getElementById("startbutton");
-    var retakebuton = document.getElementById("retake-button");
-    var confirm = document.getElementById('backpartbutton');
-    var confirm2 = document.getElementById('backpartbutton2');
-    var mediaStream = null;
-    // Prefer camera resolution nearest to 1280x720.
-    var constraints = {
-        audio: false,
-        video: {
-            width: {
-                ideal: 1920
+    (function() {
+        var camera = document.getElementById("camera");
+        var result = document.getElementById("result");
+        var video = document.getElementById("video");
+        var canvas = document.getElementById("canvas");
+        var photo = document.getElementById("photo");
+        var startbutton = document.getElementById("startbutton");
+        var retakebuton = document.getElementById("retake-button");
+        var confirm = document.getElementById('backpartbutton');
+        var confirm2 = document.getElementById('backpartbutton2');
+        var mediaStream = null;
+        // Prefer camera resolution nearest to 1280x720.
+        var constraints = {
+            audio: false,
+            video: {
+                width: {
+                    ideal: 1920
+                },
+                height: {
+                    ideal: 1080
+                },
+                facingMode: "environment"
             },
-            height: {
-                ideal: 1080
-            },
-            facingMode: "environment"
-        },
-    };
+        };
 
         async function getMediaStream(constraints) {
             try {
@@ -100,23 +97,18 @@ if(is_mobile_device==true){
                 context.drawImage(video, 0, 0, width, height);
                 var data = canvas.toDataURL('image/png');
                 photo.setAttribute('src', data);
-
-                canvas.width = widthOfCrop;// width and height of cropped image
-                canvas.height = heightOfCrop;
-                context.drawImage(video, xCrop, yCrop, widthOfCrop, heightOfCrop, 0, 0, widthOfCrop, heightOfCrop);
-                var croppedData = canvas.toDataURL("image/png", 1.0);
-
                 var front_img=sessionStorage.getItem("front_img");
                 if(front_img=='' || front_img==null){
-                 sessionStorage.setItem("front_img",croppedData);
+                 sessionStorage.setItem("front_img",111);
+                 front_image=data;
                  console.log('data111',sessionStorage.getItem("front_img"));
                 }else{
-                 sessionStorage.setItem("back_img",croppedData);
+                 sessionStorage.setItem("back_img",222);
+                 back_image=data;
                  console.log('data222',sessionStorage.getItem("back_img"));  
                  confirm.style.display = "none";
                  confirm2.style.display = "inline-block";
                 }
-
             } else {
                 clearphoto();
             }
@@ -132,12 +124,10 @@ if(is_mobile_device==true){
             var data = canvas.toDataURL('image/png');
             photo.setAttribute('src', data);
         }
-
         document.getElementById('startbutton').onclick = (event) => {
             takePicture(); 
             event.preventDefault();
         }
-
         startbutton.addEventListener("click", function(ev) {
                 camera.style.opacity = "0";
                 camera.style.visibility = "hidden";
@@ -147,7 +137,6 @@ if(is_mobile_device==true){
             },
             false
         );
-
         retakebuton.addEventListener("click", function(ev) {
                 result.style.opacity = "0";
                 result.style.visibility = "hidden";
@@ -177,17 +166,42 @@ if(is_mobile_device==true){
         );
 
         confirm2.addEventListener("click", function(ev) {
-                const pathUrlArray1 = pathUrl.split("/");
-                var pathUrlArray = pathUrlArray1.filter(function (el) {
-                    return el != '';
-                });
-                pathUrlArray.pop();
-                const newPath=pathUrlArray.join("/");
-                window.location.href=baseUrl+"take-selfie";
+                idscannerpage.style.display = "none";
+                selfiepage.style.display = "block";
+                button_callback();
         },
         false
         );
 
+        function button_callback() {
+          var video = document.getElementById("video2");
+          if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices
+              .getUserMedia({
+                video: true,
+                video: {
+                  width: {
+                    ideal: 1920,
+                  },
+                  height: {
+                    ideal: 1080,
+                  },
+                  facingMode: "user",
+                },
+              })
+              .then(function (stream) {
+                video.srcObject = stream;
+                video.setAttribute("autoplay", "");
+                video.setAttribute("muted", "");
+                video.setAttribute("playsinline", "");
+                video.play();
+              })
+              .catch(function (err0r) {
+                console.log("Something went wrong!");
+              });
+          }
+        }
+        
         window.onload = (event) => {
             switchCamera("environment");
         };
